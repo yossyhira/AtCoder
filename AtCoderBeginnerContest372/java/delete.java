53774cf5e76292f4c109a98489128e6e386bbd74
Yoshitaka Hirata <hirata@hogehoge.com>
2024-09-28 22:25:47 +0900

    fix file name

--- AtCoderBeginnerContest372/java/delete.java
+++ AtCoderBeginnerContest372/java/delete.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class a {
+public class delete {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String text = sc.next();
