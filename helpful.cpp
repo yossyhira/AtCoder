7640149828ee67cf43a02009d71da374abc7bdab
Yoshitaka Hirata <hirata@hogehoge.com>
2024-10-21 19:38:15 +0900

    practice

--- helpful.cpp
+++ helpful.cpp
@@ -13,3 +13,41 @@ int main() {
     } while (next_permutation(p.begin(), p.end()));
     return 0;
 }
+
+//2分探索(376のC)
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n-1);
+  rep(i,n) cin >> a[i];
+  rep(i,n-1) cin >> b[i];
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  auto judge = [&](int x) {
+    auto nb = b;
+    //選んだ箱追加
+    nb.push_back(x);
+    sort(nb.begin(), nb.end());
+    //大きさ合うか合わないか検索
+    rep(i,n) if (a[i] > nb[i]) return false;
+    return true;
+  };
+
+//ここが二分探索
+  const int INF = 1001001001;
+  int ac = INF, wa = 0;
+  //×と〇の境界になるまで(差が１)ループ
+  while (ac-wa > 1) {
+    int wj = (wa+ac)/2;
+    if (judge(wj)) ac = wj; else wa = wj;
+  }
+//
+  if (ac == INF) cout << -1 << endl;
+  else cout << ac << endl;
+  return 0;
+}
