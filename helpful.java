b06af25a4d144fd41e1d4a564bb2801237d037b3
Yoshitaka Hirata <hirata@hogehoge.com>
2024-11-15 22:04:03 +0900

    add helpful.java

--- helpful.java
+++ helpful.java
@@ -778,6 +778,69 @@ public class hoge {
     }
 }
 
+//二分探索(AtCoder/AtCoderBeginnerContest376/java/c.java)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n - 1];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        final int INF = 1001001001;
+        int ac = INF, wa = 0;
+
+        // ここが二分探索
+        while (ac - wa > 1) {
+            int wj = (wa + ac) / 2;
+            if (judge(wj, a, b, n)) {
+                ac = wj;
+            } else {
+                wa = wj;
+            }
+        }
+        //
+        
+        if (ac == INF) {
+            System.out.println(-1);
+        } else {
+            System.out.println(ac);
+        }
+
+        sc.close();
+    }
+
+    // Moved the judge method outside the main method
+    public static boolean judge(int x, int[] a, int[] b, int n) {
+        List<Integer> nb = new ArrayList<>();
+        for (int value : b) {
+            nb.add(value);
+        }
+        nb.add(x);
+        Collections.sort(nb);
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] > nb.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
 //輪・円問題(atcoder/AtCoderBeginnerContest376/java/b2.java)
 //基本、場合分けして数直線で考えるとよい。配列はやめた方がいい。
 //循環するときは割った余りで場所を求められる
