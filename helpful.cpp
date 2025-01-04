a67ba0eb3517751088df5518e1c308b911840b4f
Yoshitaka Hirata <hirata@hogehoge.com>
2025-01-04 19:27:47 +0900

    add

--- helpful.cpp
+++ helpful.cpp
@@ -24,6 +24,63 @@ int main() {
 210
 */
 
+//順列の例(AtCoder/cpp/054c.cpp)
+//問題(https://atcoder.jp/contests/abc054/tasks/abc054_c)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+// グラフを隣接行列で管理する
+bool G[10][10];
+
+int main() {
+    int N, M; cin >> N >> M;
+    for (int i = 0; i < M; ++i) {
+        int a, b; 
+        cin >> a >> b; 
+        --a, --b;
+        //双方向行き来できるようにする
+        G[a][b] = G[b][a] = true;
+    }
+
+    // 順列
+    vector<int> ord(N);
+    for (int i = 0; i < N; ++i) ord[i] = i;
+
+    // 順列を全部試すa
+    int res = 0;
+    do {
+
+        //今の順列の中身
+        for (int num : ord) {
+            cout << num << " ";
+        }
+        cout<<endl;
+
+        //開始が１(配列の都合で-1して0にした)かどうか確認
+        //1で無ければやり直し
+        if (ord[0] != 0) {
+            cout << "今 : " << ord[0] << endl;
+            break;
+        }
+
+        bool ok = true;
+        for (int i = 0; i + 1 < N; ++i) {
+            int from = ord[i];
+            int to = ord[i+1];
+            //途中に道が無ければfalseにする
+            if (!G[from][to]) ok = false;
+        }
+        //全て道があったら答えを1増やす
+        if (ok) ++res;
+    } while (next_permutation(ord.begin(), ord.end()));
+
+    cout << res << endl;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
 //2分探索(376のC)
 #include <bits/stdc++.h>
 using namespace std;
@@ -62,6 +119,7 @@ int main() {
   return 0;
 }
 
+//////////////////////////////////////////////////////////////////////
 //文字列操作
 #include <bits/stdc++.h>
 using namespace std;
@@ -128,6 +186,7 @@ int main() {
    */
 }
 
+////////////////////////////////////////////////////////////////////////////
 //文字列比較
 #include <bits/stdc++.h>
 using namespace std;
@@ -224,6 +283,7 @@ int main() {
     return 0;
 }
 
+/////////////////////////////////////////////////////////////////////////////
 //vectorの使い方
 //(https://qiita.com/ysuzuki19/items/df872d91c9c89cc31aee)
 //宣言
@@ -277,9 +337,6 @@ vector<int> v1{10, 9, 8};
 vector<int> v2{7, 6, 5};
 v1.insert(v1.begin() + 1, v2.begin(), v2.end()); //10 7 6 5 9 8
 
-
-
-
 //グリッド
 int h = 8, w = 8;// 8x8のグリッド
 vector<vector<char>> n(h, vector<char>(w));
@@ -291,7 +348,8 @@ for (int i = 0; i < h; i++) {
     }
 }
 
-//pair
+////////////////////////////////////////////////////////////////////////////////////////////////
+//pairの使いかた
 // 得点と名前の組を格納する配列(AtCoder/cpp/384c.cpp)
 //宣言
 vector<pair<int, string>> standings;
@@ -331,3 +389,165 @@ for (int i = 0; i < N; i++) {
     tie(a, b) = p.at(i);  
     cout << a << " " << b << endl;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//setの使い方(重複しない＆昇順に自動で並び替え)
+//宣言
+set<int> set;
+set<int> set{ 3, 4, 2, 3 };
+
+//追加
+int Idx;
+cin >> Idx;
+for (int i = 0; i < Idx; i++) {
+    int num = 0;
+    cin >> num;
+    set.insert(num);//追加
+}
+//追加出来たか確認
+//result の型は std::pair<std::set<int>::iterator, bool>
+//イテレータ（result.first）: 挿入した要素、または既存の要素を指すイテレータ
+//ブール値（result.second）: 挿入が成功した場合は true、失敗（既に存在していた場合）は false
+set<int> set;
+auto result1 = set.insert(5); // 5 を挿入
+cout << "挿入成功した？: " << result1.second << "\n"; // true
+auto result2 = set.insert(5); // 再び 5 を挿入
+cout << "挿入成功した？: " << result2.second << "\n"; // false
+
+//出力1
+for(auto v: set) {
+    cout << v << " ";
+}
+//出力2
+for (auto ite = set.begin(); ite != set.end(); ite++) {
+    cout << *ite << " ";
+}
+
+//削除
+//2という値を削除
+set.erase(2);
+
+//全要素削除
+set.clear();
+
+//値が存在するか確認
+if (set.contains(3)) {
+    // 「3」が存在するときの処理
+    //c++のコンパイラが20以降
+}
+if (set.count(1)) {
+    // 「1」が存在するときの処理
+}
+if (set.find(4) != set.end()) {
+    // 「4」が存在するときの処理
+}
+
+//最大値、最小値取得
+//自動で昇順なるので、先頭と末尾取得
+auto minIte = set.begin();
+auto maxIte = set.end() - 1;
+cout << *minIte << endl; //最小
+cout << *maxIte << endl; //最大
+
+//指定した要素目の値を取得
+//2番目の要素を取得
+int get = 2;
+ // stの先頭のイテレータを取得
+auto ite = set.begin();
+ // イテレータを2回進めて、2番目の要素を取得
+advance(ite, get);
+cout << "2番目の要素は" << *ite << std::endl;
+ //intにしたいときは直接int型に入れられる
+int ans = *ite;
+cout << "2番目の要素は" << ans << std::endl;
+
+
+//指定した値がどの要素にあるか取得(要素は0始まり)
+// 5を探す
+//イテレータを使う(https://atcoder.jp/contests/apg4b/tasks/APG4b_ai)
+//簡単にいうと、要素の値が何か(例：num[1])を指すもの。そして、どの場所(1番目)にあったかも覚えてる
+auto it = find(set.begin(), set.end(), 5);
+cout << *it << endl;
+int ans = (it != set.end()) ? distance(set.begin(), it) : -1; 
+cout << ans << endl;
+
+//逆順出力
+// リバースイテレータで逆順に出力(reverse(set.begin(), set.end());は使えない)
+for (auto it = set.rbegin(); it != set.rend(); ++it) {
+    cout << *it << " ";
+}
+//vectorにしてreverseする
+vector<int> vec(st.begin(), st.end());
+reverse(vec.begin(), vec.end());
+// 出力
+for (int val : vec) {
+    std::cout << val << " ";
+}
+
+//もしくはカスタム(詳しくは下)
+struct ReverseCompare {
+    bool operator()(int a, int b) const {
+        return a > b; // 降順
+    }
+};
+int main() {
+    multiset<int, ReverseCompare> set; //自動で降順
+}
+
+//並び順をカスタムしたい
+#include <bits/stdc++.h>
+using namespace std;
+
+// カスタム比較関数
+struct CustomCompare {
+    bool operator()(int a, int b) const {
+        if ((a % 2 != b % 2)) { 
+            return a % 2 > b % 2; // 奇数が先になるように並べていく
+        }
+        return a < b; // 奇数同士・偶数同士なら昇順
+    }
+};
+
+int main() {
+    int Idx;
+    cin >> Idx;
+
+    multiset<int, CustomCompare> multiset; // カスタム比較関数を適用
+    for (int i = 0; i < Idx; i++) {
+        int num = 0;
+        cin >> num;
+        multiset.insert(num); //7 2 4 5 9 6
+    }
+
+    // 出力
+    for (auto v : multiset) {
+        cout << v << " "; //5 7 9 2 4 6 <- 奇数先で、奇数同士・偶数同士なら昇順なってる
+    }
+
+    cout << "\n";
+}
+
+//setでpair使うsetpair
+// pair<int, int> を格納するセット
+set<pair<int, int>> st;
+for (int i = 0; i < Idx; i++) {
+    int num = 0, num2 = 0;
+    cin >> num >> num2;
+    st.insert({num, num2});
+}
+
+// セット内のペアを順番に出力
+for (auto it = st.begin(); it != st.end(); ++it) {
+    cout << "(" << it->first << ", " << it->second << ") ";
+}
+
+//setをvectorに変換
+vector<int> vec(st.begin(), st.end());
+//setpairをvec
+vector<pair<int, int>> vec(st.begin(), st.end());
+
+//vectorからset
+//重複取り除かれて、昇順なってる
+set<int> st(vec.begin(), vec.end());
+//vecをsetにする
+set<pair<int, int>> st(vec.begin(), vec.end());
