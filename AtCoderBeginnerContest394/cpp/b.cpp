24811ef7ff0a2afe47128721ab95574e8d7da85b
Yoshitaka Hirata <hirata@hogehoge.com>
2025-02-22 17:47:31 +0900

    contest

---
+++ AtCoderBeginnerContest394/cpp/b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b, c;
+    string text;
+    cin >> a >> b >> c >> text;
+    cout << << endl;
+}
