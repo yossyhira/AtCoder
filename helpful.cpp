dab69c1ee8aef745d7c6c33021ba5420e156ffc9
Yoshitaka Hirata <hirata@hogehoge.com>
2025-04-18 15:59:57 +0900

    add helpful 01BFS

--- helpful.cpp
+++ helpful.cpp
@@ -973,6 +973,78 @@ int main() {
     return 0;
 }
 
+//01BFS/////////////////////////////////////////////////////////
+//01BFS(普通のBFSとダイクストラの概念を混ぜた感じ)
+//普通の場合と特殊な場合の2パターンがあるBFS
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int di[] = {-1,0,1,0};
+int dj[] = {0,-1,0,1};
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i,h) cin >> s[i];
+
+  int si, sj, ti, tj;
+  cin >> si >> sj >> ti >> tj;
+  --si; --sj; --ti; --tj;
+
+  const int INF = 1001001001;
+  vector<vector<int>> dist(h,vector<int>(w, INF));
+  vector<vector<bool>> used(h,vector<bool>(w));
+  deque<pair<int,int>> q;
+
+  // i, j : 座標
+  // d : その頂点までの総距離（コスト）
+  // cost : その辺単体の重み(dequeの前か後のどっちに入れるのか判別に使う)
+  auto push = [&](int i, int j, int d, int cost) {
+    //最短距離更新するかどうか
+    if (dist[i][j] <= d) return;
+    dist[i][j] = d;
+    //コストが0なら前，1なら後ろに追加
+    if (cost == 0) q.emplace_front(i,j);
+    else q.emplace_back(i,j);
+  };
+  push(si,sj,0,0);
+
+  while (q.size()) {
+    //最短距離で取り出しているので，見てなければ距離確定
+    int i, j;
+    tie(i, j) = q.front(); q.pop_front();
+    if (used[i][j]) continue;
+    //その座標を見たにする
+    used[i][j] = true;
+    //その座標の距離
+    int d = dist[i][j];
+
+    //普通の道(コスト0)
+    rep(v,4) {
+      int ni = i+di[v], nj = j+dj[v];
+      if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
+      if (s[ni][nj] == '.') push(ni,nj,d,0);
+    }
+    //壁の道(コスト1)
+    //普通の道でも壁とみなして移動する
+    //普通の道の方はif (used[i][j]) continue;かコスト0なのでpush関数のif (dist[i][j] <= d) return;ではじいてくれる
+    rep(v,4) {
+      int ni = i, nj = j;
+      //壁の2マス分同じ方向に移動
+      rep(k,2) {
+        ni += di[v]; nj += dj[v];
+        if (ni < 0 || nj < 0 || ni >= h || nj >= w) break;
+        push(ni,nj,d+1,1);
+      }
+    }
+  }
+
+  int ans = dist[ti][tj];
+  cout << ans << endl;
+  return 0;
+}
 
 //グラフの問題(各頂点までの距離)//////////////////////////////////
 //問題(https://qiita.com/drken/items/996d80bcae64649a6580)
@@ -1985,6 +2057,9 @@ int main() {
     }
     cout << ans << endl;
 }
+//2乗////////////////////////////////////////////////////////////////////////
+const ll max = 1ll<<60;//2^60
+
 //longの計算///////////////////////////////////////////////////////////
 long = long + int;//できる(intを暗黙の型変換してくれる)
 long = int + int;//int + int でintの範囲外になるとオーバーフローする
