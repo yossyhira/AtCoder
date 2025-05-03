356b38a00e07f5fcd803b4a2820f7d3770b38532
Yoshitaka Hirata <hirata@hogehoge.com>
2025-05-03 20:27:04 +0900

    practise

---
+++ cpp/353a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+//#define eb emplace_back
+//#define em emplace
+//#define pob pop_back
+//using ld = long double;
+using ll = long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int base;
+    cin >> base;
+    for (int i = 1; i < n; i++) {
+        int num;
+        cin >> num;
+        if(base < num){
+            cout << i+1 << endl;
+            return 0;
+        }
+    }
+    dame;
+}
---
+++ cpp/361a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+//#define eb emplace_back
+//#define em emplace
+//#define pob pop_back
+//using ld = long double;
+using ll = long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k, x;
+    cin >> n >> k >> x;
+    k --;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        cout << num << " ";
+        if(i == k) cout << x << " ";
+    }
+    cout << endl;
+
+}
