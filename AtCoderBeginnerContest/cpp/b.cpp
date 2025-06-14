331b7f1f66fadeaeebd5f215148b874a6e3566f4
Yoshitaka Hirata <hirata@hogehoge.com>
2025-06-14 20:29:52 +0900

    add main

--- AtCoderBeginnerContest/cpp/b.cpp
+++ AtCoderBeginnerContest/cpp/b.cpp
@@ -13,7 +13,9 @@ using LP = pair<ll, ll>;
 const ll LINF = 1001002003004005006ll;
 const int INF = 1001001001;
 #define yes cout<<"Yes"<<endl
+#define yesr {cout<<"Yes"<<endl; return 0;}
 #define no cout<<"No"<<endl
+#define nor {cout<<"No"<<endl; return 0;}
 #define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
 #define dame cout<<-1<<endl
 #define chmax(x,y) x = max(x,y)
