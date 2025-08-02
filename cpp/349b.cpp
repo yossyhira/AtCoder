36eacde5c7b347603f78f0bae9ee8a4c928cb00b
Yoshitaka Hirata <hirata@hogehoge.com>
2025-08-02 19:31:17 +0900

    practise

---
+++ cpp/349b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+//#define eb emplace_back
+//#define em emplace
+//#define pob pop_back
+//using ld = long double;
+using ll = long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+#define yes cout<<"Yes"<<endl
+#define yesr {cout<<"Yes"<<endl; return 0;}
+#define no cout<<"No"<<endl
+#define nor {cout<<"No"<<endl; return 0;}
+#define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+
+    vector<int> c(26, 0);
+    for (int i = 0; i < s.size(); i++) {
+        c[s[i] - 'a'] ++;
+    }
+    for (int i = 1; i <= 100; i++) {
+        int cnt = 0;
+        for (int j = 0; j < 26; j++) {
+            if(c[j] == i) cnt ++;
+        }
+        if(cnt != 0 && cnt != 2) nor
+    }
+    yes;
+}
