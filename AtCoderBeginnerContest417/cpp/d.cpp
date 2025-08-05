78e10e52699468933c254666c01a6cc24bb52b6d
Yoshitaka Hirata <hirata@hogehoge.com>
2025-08-05 23:54:57 +0900

    practise

--- AtCoderBeginnerContest417/cpp/d.cpp
+++ AtCoderBeginnerContest417/cpp/d.cpp
@@ -26,15 +26,52 @@ int main() {
     cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<tuple<int, int, int>> p;
+    vector<int> p(n);
+    vector<int> a(n);
+    vector<int> b(n);
+    vector<int> sum_b(n+1, 0); 
     for (int i = 0; i < n; i++) {
-        cin >> p >> a >> b;
-        p.pb({p, a, b});
+        cin >> p[i] >> a[i] >> b[i];
+        sum_b[i+1] = sum_b[i] + b[i];
     }
+    //cout << sum_b[n] << endl;
+    const int t = 1000;
+    vector<vector<int>> dp(n, vector<int>(t+1, 0));
+    for (int j = 0; j <= t; j++) {
+        if(j <= p[n-1]) dp[n-1][j] = a[n-1] + j;
+        else dp[n-1][j] = max(j - b[n-1], 0);
+    }
+    for (int i = n-2; i >= 0; i--) {
+        for (int j = 0; j <= t; j++) {
+            int num;
+            if(j <= p[i]) num = a[i] + j;
+            else num = max(j - b[i], 0);
+            dp[i][j] = dp[i + 1][num];
+        }
+    }
+    auto f = [&](int r) {
+        int pos = upper_bound(sum_b.begin(), sum_b.end(), r) - sum_b.begin();
+        return pos;
+    };
     int q;
     cin >> q;
     for (int qi = 0; qi < q; qi++) {
         int x;
         cin >> x;
+        int idx = 0;
+        int ans = 0;
+        if(t < x) {
+            idx = f(x - t);
+            if(n <= idx){
+                ans = x - sum_b[n];
+            }else{
+                int j = x - sum_b[idx];
+                ans = dp[idx][j];
+            }
+        }else{
+            ans = dp[idx][x];
+        }
+        cout << ans << endl;
+
     }
 }
