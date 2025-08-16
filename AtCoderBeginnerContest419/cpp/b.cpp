cf04d008ab863906b4d3143aada7b1ccc3a30725
Yoshitaka Hirata <hirata@hogehoge.com>
2025-08-16 23:40:55 +0900

    contest

---
+++ AtCoderBeginnerContest419/cpp/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+//#define eb emplace_back
+//#define em emplace
+//#define pob pop_back
+//using ld = long double;
+using ll = long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+#define yes cout<<"Yes"<<endl
+#define yesr {cout<<"Yes"<<endl; return 0;}
+#define no cout<<"No"<<endl
+#define nor {cout<<"No"<<endl; return 0;}
+#define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    priority_queue<int, vector<int>, greater<int>> q;//小さい順(昇順)に取り出す
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if(x == 1){
+            int a;
+            cin >> a;
+            q.emplace(a);
+        }else{
+            int a = q.top(); q.pop();
+            cout << a << endl;
+        }
+    }
+}
