33da2a73ffb1632abd60c70ed7f5b264abdceccf
Yoshitaka Hirata <hirata@hogehoge.com>
2025-10-12 23:08:21 +0900

    practise

---
+++ AtCoderBeginnerContest427/cpp/c3.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define fi first
+#define se second
+#define pb push_back
+//#define eb emplace_back
+//#define em emplace
+//#define pob pop_back
+//using ld = long double;
+using ll = long long;
+using P = pair<int, int>;
+using LP = pair<ll, ll>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+#define yes cout<<"Yes"<<endl
+#define yesr {cout<<"Yes"<<endl; return 0;}
+#define no cout<<"No"<<endl
+#define nor {cout<<"No"<<endl; return 0;}
+#define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+#define chmax(x,y) x = max(x,y)
+#define chmin(x,y) x = min(x,y)
+#define all(x) begin(x), end(x)
+const int mod = 1e9 + 7;
+using Graph = vector<vector<int>>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 頂点数と辺数
+    int N, M; cin >> N >> M;
+    dsu uf(N);
+    // グラフ入力受取 (ここでは無向グラフを想定)
+    Graph G(N);
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+        uf.merge(a,b);
+    }
+
+    // BFS のためのデータ構造
+    auto bfs = [&](int s){
+        vector<int> dist(N, -1); // 全頂点を「未訪問」に(-1)初期化
+        vector<bool> clr(N, false);
+        int ans = 0;
+        queue<int> que; 
+    
+        // 初期条件 (頂点 0 を初期ノードとする)
+        //dist[v] はスタート頂点から頂点 v まで最短何ステップで到達できるかを表す
+        int start = s;
+        dist[start] = 0; //スタートは0ステップ
+        clr[s] = false;
+        //その時点での橙色頂点 (発見済みだが未訪問な頂点) を格納するキュー
+        que.push(start); // スタートを橙色(自分からいける場所を見たい)頂点にする
+    
+        // BFS 開始 (キューが空になるまで探索を行う)
+        while (!que.empty()) {
+            int v= que.front(); // キューから先頭頂点を取り出す
+            que.pop(); //先頭削除
+            // 自分（v） から辿れる頂点をすべて調べる
+            //cout <<"v" <<v+1 << endl;
+            for (int nv : G[v]) {
+                //cout <<"nv" <<nv +1<< endl;
+                if (dist[nv] != -1){
+                    if(clr[nv] == clr[v]) ans ++;
+                    //cout << "ans"<< ans << "nv" << nv+1 << endl;
+                    continue;
+                } // すでに発見済み(ステップ数が分かってる)の頂点は探索しない
+                clr[nv] = (clr[v] ? false : true);
+                // 新たな白色頂点 nv について距離情報を更新してキューに追加する
+                //distは各頂点の最短ステップ数
+                dist[nv] = dist[v] + 1; //自分がいる所の次なので自分の場所から1ステップ増やす
+                que.push(nv);//発見した(そこまでのステップ数が分かった)ので
+                             //自分からいける場所を後で調べる
+            }
+        }
+        return ans;
+
+    };
+    vector<vector<int>> num = uf.groups();
+    int ans = 0;
+    for (int i = 0; i < num.size(); i++) {
+        int mn = INF;
+        for (int j = 0; j < num[i].size(); j++) {
+            chmin(mn, bfs(num[i][j]));
+        }
+        ans += (mn/2);
+    }
+    cout << ans << endl;
+    // 結果出力 (各頂点の頂点 0 からの距離を見る)
+    //for (int v = 0; v < N; ++v) cout << v << ": " << dist[v] << endl;
+}
+/*
+4 6
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+*/
